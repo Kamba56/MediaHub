5aa087e8d1637f97fc9b5cf7737c1082f56c3b5e
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-06-23 17:34:47 +0430

    Add popup menu

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './css/fontawesome.css';
 import './js/bootstrap.js';
 import getList from './modules/apiLink.js';
 import './style.css';
+import './modules/pop-up.js';
 
 getList();
---
+++ src/modules/pop-up.js
@@ -0,0 +1,12 @@
+
+
+const list = document.querySelector('#pop');
+
+
+const popup = (movie, myBtn) => {
+  myBtn.addEventListener('click', () => {
+    console.log('I work');
+  });
+}
+
+export default popup;

2d28503f4fac84b50d38990be5f809eabc006971
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-06-24 16:38:51 +0430

    Add new likes

--- src/modules/displayComments.js
+++ src/modules/displayComments.js
@@ -2,8 +2,9 @@ const displayCommments = async (itemId) => {
   const options = {
     method: 'GET',
   };
-  const data = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/6tnbAuztHu5w2iAn4Bga/comments?item_id=${itemId}`, options);
-  return data.json();
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/6tnbAuztHu5w2iAn4Bga/comments?item_id=${itemId}`, options)
+    .then((data) => data.json(), () => '');
+  return response;
 };
 
 export default displayCommments;

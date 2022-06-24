1a5932fe24bf084a25acd393a2605f18b6ff2a18
Kamba56 <tomajohn316@yahoo.com>
2022-06-24 13:32:32 +0100

    Update displayComments.js

--- src/modules/pop-up.js
+++ src/modules/pop-up.js
@@ -4,16 +4,16 @@ const popUp = document.querySelector('#pop-up');
 
 const popup = async (movies) => {
   const commentBtn = document.querySelectorAll('.my-btn');
-  commentBtn.forEach(async (btn) => {
-    btn.addEventListener('click', () => {
+  await commentBtn.forEach(async (btn) => {
+    await btn.addEventListener('click', async () => {
       const btnID = btn.id;
-      movies.forEach(async (movie) => {
-        const comments = await displayComments(movie.imdbid);
-        const store = await comments.map((elem) => `<h4>${elem.username}</h4>
-          <p>${elem.comment}</p>
-          <p>${elem.creation_date}</p>
-          `).join('');
+      await movies.forEach(async (movie) => {
         if (movie.imdbid === btnID) {
+          const comments = await displayComments(movie.imdbid);
+          const store = (comments === '') ? '' : comments.map((elem) => `<h4>${elem.username}</h4>
+            <p>${elem.comment}</p>
+            <p>${elem.creation_date}</p>
+            `).join('');
           popUp.style.display = 'block';
           popUp.innerHTML = `
           <div class="popup-design">
@@ -27,11 +27,11 @@ const popup = async (movies) => {
           <div class="text-light comments">${store}</div>
           </div>    
           `;
+          const btnClose = document.querySelector('.btnClose');
+          btnClose.addEventListener('click', () => {
+            popUp.style.display = 'none';
+          });
         }
-        const btnClose = document.querySelector('.btnClose');
-        btnClose.addEventListener('click', () => {
-          popUp.style.display = 'none';
-        });
       });
     });
   });

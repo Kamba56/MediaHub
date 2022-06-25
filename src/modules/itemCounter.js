86ad0ff00e94903d8ef71a48424a3d4d84517f54
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-06-25 11:51:27 +0430

    Add some good practices in item counter file

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,5 +1,6 @@
 const items = () => {
   const list = document.querySelector('#list');
+  // take the movie list from html elements
   const movieList = Array.from(list.querySelectorAll('.grid-item'));
   document.getElementById('num').innerHTML = movieList.length;
 };
